978703e5d86c1bfefe04d0fd46347bdd90312505
Kevin <klanger@ccs.neu.edu>
2015-03-18 22:24:38 -0400

    Really just more boiler plate, but regardless functional NT. BTB hooked up but not doing much

--- branch_pred/btb.c
+++ branch_pred/btb.c
@@ -1,20 +1,42 @@
 #include "btb.h"
 
 entry_t * btb_init(int pwr2_sz) { 
-    btb_state = malloc(sizeof(metadata_t));
-    int size = pow(2,pwr2_sz);  
-    btb_state->entries = (entry_t *)malloc(sizeof(entry_t)*size);
-    btb_state->sz = pwr2_sz;
+    int size = pow(2,pwr2_sz); 
+    btb_state.entries = (entry_t *)malloc(sizeof(entry_t)*size);
+    btb_state.sz = pwr2_sz;
+    return btb_state.entries;
 } 
-void btb_train(int index, unsigned long long target) { 
-}
 btb_index_t btb_index(unsigned long long pc) {
-    btb_index_t idx; 
-    unsigned long long pcsft = pc>>PREDWIND;
-    long long sz = 32; 
+    btb_index_t idx;
+    long long fetchwindow = pow(2,6)-1; 
+    idx.offset = fetchwindow&pc; 
+    unsigned long long pcsft = pc>>6;
+    long long sz = pow(2,PREDWIND); 
     int msk = sz-1; 
     idx.index = msk&pcsft; 
     idx.tag = ( (~msk)&pcsft ) >> ilog2(sz);    
     return idx;
+}
+entry_t btb_read(entry_t * btb, unsigned long long pc) {
+    btb_index_t idx = btb_index(pc); 
+    unsigned long long tag, target; 
+    entry_t btbEntry = btb[idx.index];
+    return btbEntry; 
+}   
+unsigned long long btb_readTgt(entry_t btbEntry, unsigned long long pc) {
+    btb_index_t idx = btb_index(pc); 
+    unsigned long long tag, target; 
+    target = btbEntry.target; 
+    tag = btbEntry.tag; 
+    if (btbEntry.val && tag == idx.tag) {
+        return target; 
+    }
+    return 0;
+}   
+void btb_train(entry_t * btb, btb_index_t idx, unsigned long long target) { 
+    entry_t * btbEntry = &btb[idx.index];
+    btbEntry->target = target; 
+    btbEntry->tag = idx.tag; 
+    btbEntry->val = TRUE;
 }
 //Model of the BTB
--- branch_pred/main.c
+++ branch_pred/main.c
@@ -1,24 +1,14 @@
 #include "btb.h"
 #include "decode.h"
 #include "readva.h" 
-
+#include "engine.h"
 int main() {
     //Parse trace 
     char * inFileString = "trace.txt"; 
     FILE *inFilePtr = fopen(inFileString, "r");
     istream_t * VAs = readimem(inFilePtr); 
-    unsigned long long pc;
-    char * instr;  
-    int num = 3104; 
-    printf("%d\n",btb_index(num).index);
-    printf("%d\n",btb_index(num).tag);
-    while (VAs) {
-        if (VAs->pc != pc) {
-     //       printf("%llx %s\n",VAs->pc,VAs->instr); 
-        }
-        instr = VAs->instr; 
-        pc = VAs->pc+VAs->ilen; 
-        VAs = VAs->next; 
-    }
+
+    entry_t * btb = btb_init(5);
+    run(VAs,btb);    
     return 0;
 }
